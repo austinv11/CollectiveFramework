6710dbce47d4855db711ccedacc1fb056b61b1c6
austinv11 <austinv11@gmail.com>
2015-03-27 18:56:52 -0400

    Did I say how much I hate bash?

--- droneDeploy.sh
+++ droneDeploy.sh
@@ -4,4 +4,4 @@ echo "$CF_VERSION"
 FILENAME="CollectiveFramework-$CF_VERSION-deobf.jar"
 LOCAL_FILENAME="build/libs/$FILENAME"
 URL="https://api.bintray.com/content/austinv11/maven/CollectiveFramework/$CF_VERSION/$FILENAME"
-curl -T LOCAL_FILENAME -uaustinv11:"$API_KEY" URL
+curl -T "$LOCAL_FILENAME" -uaustinv11:"$API_KEY" "$URL"
