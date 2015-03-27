9229749e2091b0966587ebad672005238eda4693
austinv11 <austinv11@gmail.com>
2015-03-27 18:27:25 -0400

    Hm let's try this

---
+++ droneDeploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+KEY=API_KEY
+VERSION=CF_VERSION
+FILENAME="CollectiveFramework-$VERSION-deobf.jar"
+LOCAL_FILENAME="build/libs/$FILENAME"
+URL="https://api.bintray.com/content/austinv11/maven/CollectiveFramework/$VERSION/$FILENAME"
+curl -T LOCAL_FILENAME -uaustinv11:"$KEY" URL
