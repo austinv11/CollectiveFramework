8b70a111fdc4fff3433be3ac23367bb165e03106
austinv11 <austinv11@gmail.com>
2015-05-28 17:29:42 -0400

    The player must now be op for them to be able to change time

--- src/main/java/com/austinv11/collectiveframework/minecraft/event/TickHandler.java
+++ src/main/java/com/austinv11/collectiveframework/minecraft/event/TickHandler.java
@@ -10,6 +10,7 @@ import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
+import net.minecraft.server.MinecraftServer;
 
 public class TickHandler {
 	
@@ -22,31 +23,35 @@ public class TickHandler {
 	@SubscribeEvent
 	public void onClientTick(TickEvent.ClientTickEvent event) {
 		if (Config.enableButtonTimeChanging) {
-			if (Minecraft.getMinecraft().theWorld != null && LogicUtils.xor(Keybindings.TIME_BACK.getIsKeyPressed(), Keybindings.TIME_FORWARD.getIsKeyPressed())) {
-				if (startWorldTime == -1)
-					startWorldTime = Minecraft.getMinecraft().theWorld.getWorldTime();
-				if (startCloudTicks == -1)
-					startCloudTicks = Minecraft.getMinecraft().renderGlobal.cloudTickCounter;
-				boolean isForward;
-				if (Keybindings.TIME_BACK.getIsKeyPressed())
-					isForward = false;
-				else
-					isForward = true;
-				
-				wasDown = true;
-				int difference = (isForward ? Config.timeChangeRate : -Config.timeChangeRate);
-				totalTimeChange += difference;
-				Minecraft.getMinecraft().theWorld.setWorldTime(startWorldTime+totalTimeChange);
-				Minecraft.getMinecraft().renderGlobal.cloudTickCounter = startCloudTicks+totalTimeChange;
-			}
-			
-			if (wasDown && Minecraft.getMinecraft().theWorld != null && LogicUtils.nor(Keybindings.TIME_BACK.getIsKeyPressed(),
-					Keybindings.TIME_FORWARD.getIsKeyPressed())) {
-				CollectiveFramework.NETWORK.sendToServer(new TimeUpdatePacket(startWorldTime, totalTimeChange));
-				startWorldTime = -1;
-				totalTimeChange = 0;
-				startCloudTicks = -1;
-				wasDown = false;
+			if (Minecraft.getMinecraft().theWorld != null) {
+				if (MinecraftServer.getServer().getConfigurationManager().func_152596_g(Minecraft.getMinecraft().thePlayer.getGameProfile())) {
+					if (LogicUtils.xor(Keybindings.TIME_BACK.getIsKeyPressed(), Keybindings.TIME_FORWARD.getIsKeyPressed())) {
+						if (startWorldTime == -1)
+							startWorldTime = Minecraft.getMinecraft().theWorld.getWorldTime();
+						if (startCloudTicks == -1)
+							startCloudTicks = Minecraft.getMinecraft().renderGlobal.cloudTickCounter;
+						boolean isForward;
+						if (Keybindings.TIME_BACK.getIsKeyPressed())
+							isForward = false;
+						else
+							isForward = true;
+						
+						wasDown = true;
+						int difference = (isForward ? Config.timeChangeRate : -Config.timeChangeRate);
+						totalTimeChange += difference;
+						Minecraft.getMinecraft().theWorld.setWorldTime(startWorldTime+totalTimeChange);
+						Minecraft.getMinecraft().renderGlobal.cloudTickCounter = startCloudTicks+totalTimeChange;
+					}
+					
+					if (wasDown && LogicUtils.nor(Keybindings.TIME_BACK.getIsKeyPressed(),
+							Keybindings.TIME_FORWARD.getIsKeyPressed())) {
+						CollectiveFramework.NETWORK.sendToServer(new TimeUpdatePacket(startWorldTime, totalTimeChange));
+						startWorldTime = -1;
+						totalTimeChange = 0;
+						startCloudTicks = -1;
+						wasDown = false;
+					}
+				}
 			}
 		}
 	}
