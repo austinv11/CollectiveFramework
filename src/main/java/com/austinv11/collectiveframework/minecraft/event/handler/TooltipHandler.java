ff96815daa672885a92929dba09d265459bcbae6
austinv11 <austinv11@gmail.com>
2015-06-04 19:55:56 -0400

    Added an unlocalised name tool tip

--- src/main/java/com/austinv11/collectiveframework/minecraft/event/handler/TooltipHandler.java
+++ src/main/java/com/austinv11/collectiveframework/minecraft/event/handler/TooltipHandler.java
@@ -3,6 +3,9 @@ package com.austinv11.collectiveframework.minecraft.event.handler;
 import com.austinv11.collectiveframework.minecraft.reference.Config;
 import com.austinv11.collectiveframework.minecraft.utils.Colors;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.oredict.OreDictionary;
@@ -15,8 +18,9 @@ public class TooltipHandler {
 	@SubscribeEvent
 	public void onTooltip(ItemTooltipEvent event) {
 		if (Config.debugTooltips) {
-			event.toolTip.addAll(getNBTTooltip(event.itemStack));
+			event.toolTip.addAll(getUnlocalisedName(event.itemStack));
 			event.toolTip.addAll(getOreDictTooltip(event.itemStack));
+			event.toolTip.addAll(getNBTTooltip(event.itemStack));
 		}
 	}
 	
@@ -36,6 +40,19 @@ public class TooltipHandler {
 			for (int id : OreDictionary.getOreIDs(stack)) {
 				tooltip.add("-"+OreDictionary.getOreName(id)+" (id: "+id+")");
 			}
+		}
+		return tooltip;
+	}
+	
+	private List<String> getUnlocalisedName(ItemStack stack) {
+		List<String> tooltip = new ArrayList<String>();
+		tooltip.add(Colors.UNDERLINE+"String Item ID:");
+		Item item = stack.getItem();
+		if (item instanceof ItemBlock) {
+			Block block = Block.getBlockFromItem(item);
+			tooltip.add(Block.blockRegistry.getNameForObject(block));
+		} else {
+			tooltip.add(Item.itemRegistry.getNameForObject(item));
 		}
 		return tooltip;
 	}
